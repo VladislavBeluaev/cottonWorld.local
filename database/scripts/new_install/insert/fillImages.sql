4db22e47efeed3fb2513e08742f85d5b56cff778
Stepan <paradox_on@mail.ru>
2019-04-30 00:21:54 +0300

    add women images

--- database/scripts/new_install/insert/fillImages.sql
+++ database/scripts/new_install/insert/fillImages.sql
@@ -12,3 +12,43 @@ INSERT INTO images(`img_src`,`img_alt`,`created_at`,`updated_at`) values('images
 INSERT INTO images(`img_src`,`img_alt`,`created_at`,`updated_at`) values('images/men/t_shirts/futbolka-long_sleeve/1555783482.jpg','futbolka-long_sleeve',now(),now());
 INSERT INTO images(`img_src`,`img_alt`,`created_at`,`updated_at`) values('images/men/t_shirts/polo-short_sleeve/1555783498.jpg','polo-short_sleeve',now(),now());
 INSERT INTO images(`img_src`,`img_alt`,`created_at`,`updated_at`) values('images/men/t_shirts/polo-long-sleeve/1555783543.jpg','polo-long-sleeve',now(),now());
+
+
+--women img
+
+INSERT INTO images(`img_src`,`img_alt`,`created_at`,`updated_at`) values('images/women/t_shirts/promo-futbolka_cotton_short_sleeve/1556567586.jpg','promo-futbolka_cotton_short_sleeve',now(),now());
+
+INSERT INTO images(`img_src`,`img_alt`,`created_at`,`updated_at`) values('images/women/t_shirts/promo-futbolka_cotton_short_sleeve/1556568035.jpg','promo-futbolka_cotton_short_sleeve',now(),now());
+
+INSERT INTO images(`img_src`,`img_alt`,`created_at`,`updated_at`) values('images/women/t_shirts/promo-futbolka_cotton_short_sleeve/1556568052.jpg','promo-futbolka_cotton_short_sleeve',now(),now());
+
+INSERT INTO images(`img_src`,`img_alt`,`created_at`,`updated_at`) values('images/women/t_shirts/promo-futbolka_cotton_short_sleeve/1556568071.jpg','promo-futbolka_cotton_short_sleeve',now(),now());
+
+INSERT INTO images(`img_src`,`img_alt`,`created_at`,`updated_at`) values('images/women/t_shirts/promo-futbolka_cotton_short_sleeve/1556568084.jpg','promo-futbolka_cotton_short_sleeve',now(),now());
+
+INSERT INTO images(`img_src`,`img_alt`,`created_at`,`updated_at`) values('images/women/t_shirts/promo-futbolka_cotton_short_sleeve/1556568094.jpg','promo-futbolka_cotton_short_sleeve',now(),now());
+
+INSERT INTO images(`img_src`,`img_alt`,`created_at`,`updated_at`) values('images/women/t_shirts/promo-futbolka_cotton_short_sleeve/1556568106.jpg','promo-futbolka_cotton_short_sleeve',now(),now());
+
+INSERT INTO images(`img_src`,`img_alt`,`created_at`,`updated_at`) values('images/women/t_shirts/promo-futbolka_cotton_short_sleeve/1556568118.jpg','promo-futbolka_cotton_short_sleeve',now(),now());
+
+
+--c lycra short sleeve
+
+INSERT INTO images(`img_src`,`img_alt`,`created_at`,`updated_at`) values('images/women/t_shirts/promo-futbolka_ lycra_short_sleeve/1556568512.jpg','promo-futbolka_ lycra_short_sleeve',now(),now());
+
+INSERT INTO images(`img_src`,`img_alt`,`created_at`,`updated_at`) values('images/women/t_shirts/promo-futbolka_ lycra_short_sleeve/1556568527.jpg','promo-futbolka_ lycra_short_sleeve',now(),now());
+
+INSERT INTO images(`img_src`,`img_alt`,`created_at`,`updated_at`) values('images/women/t_shirts/promo-futbolka_ lycra_short_sleeve/1556568542.jpg','promo-futbolka_ lycra_short_sleeve',now(),now());
+
+INSERT INTO images(`img_src`,`img_alt`,`created_at`,`updated_at`) values('images/women/t_shirts/promo-futbolka_ lycra_short_sleeve/1556568552.jpg','promo-futbolka_ lycra_short_sleeve',now(),now());
+
+INSERT INTO images(`img_src`,`img_alt`,`created_at`,`updated_at`) values('images/women/t_shirts/promo-futbolka_ lycra_short_sleeve/1556568562.jpg','promo-futbolka_ lycra_short_sleeve',now(),now());
+
+INSERT INTO images(`img_src`,`img_alt`,`created_at`,`updated_at`) values('images/women/t_shirts/promo-futbolka_ lycra_short_sleeve/1556568571.jpg','promo-futbolka_ lycra_short_sleeve',now(),now());
+
+INSERT INTO images(`img_src`,`img_alt`,`created_at`,`updated_at`) values('images/women/t_shirts/promo-futbolka_ lycra_short_sleeve/1556568581.jpg','promo-futbolka_ lycra_short_sleeve',now(),now());
+
+INSERT INTO images(`img_src`,`img_alt`,`created_at`,`updated_at`) values('images/women/t_shirts/promo-futbolka_ lycra_short_sleeve/1556568592.jpg','promo-futbolka_ lycra_short_sleeve',now(),now());
+
+
--- database/scripts/new_install/insert/fillT_shirt_image.sql
+++ database/scripts/new_install/insert/fillT_shirt_image.sql
@@ -25,3 +25,60 @@ INSERT INTO t_shirt_image(`t_shirt_id`,`image_id`,`created_at`,`updated_at`)
 values(4,4,now(),now());
 INSERT INTO t_shirt_image(`t_shirt_id`,`image_id`,`created_at`,`updated_at`)
 values(5,5,now(),now());
+
+
+
+--women
+
+INSERT INTO t_shirt_image(`t_shirt_id`,`image_id`,`created_at`,`updated_at`)
+values(6,12,now(),now());
+
+INSERT INTO t_shirt_image(`t_shirt_id`,`image_id`,`created_at`,`updated_at`)
+values(6,13,now(),now());
+
+INSERT INTO t_shirt_image(`t_shirt_id`,`image_id`,`created_at`,`updated_at`)
+values(6,14,now(),now());
+
+INSERT INTO t_shirt_image(`t_shirt_id`,`image_id`,`created_at`,`updated_at`)
+values(6,15,now(),now());
+
+INSERT INTO t_shirt_image(`t_shirt_id`,`image_id`,`created_at`,`updated_at`)
+values(6,16,now(),now());
+
+INSERT INTO t_shirt_image(`t_shirt_id`,`image_id`,`created_at`,`updated_at`)
+values(6,17,now(),now());
+
+INSERT INTO t_shirt_image(`t_shirt_id`,`image_id`,`created_at`,`updated_at`)
+values(6,18,now(),now());
+
+INSERT INTO t_shirt_image(`t_shirt_id`,`image_id`,`created_at`,`updated_at`)
+values(6,19,now(),now());
+
+
+
+--lycra_shortt sleeve
+
+
+INSERT INTO t_shirt_image(`t_shirt_id`,`image_id`,`created_at`,`updated_at`)
+values(7,20,now(),now());
+
+INSERT INTO t_shirt_image(`t_shirt_id`,`image_id`,`created_at`,`updated_at`)
+values(7,21,now(),now());
+
+INSERT INTO t_shirt_image(`t_shirt_id`,`image_id`,`created_at`,`updated_at`)
+values(7,22,now(),now());
+
+INSERT INTO t_shirt_image(`t_shirt_id`,`image_id`,`created_at`,`updated_at`)
+values(7,23,now(),now());
+
+INSERT INTO t_shirt_image(`t_shirt_id`,`image_id`,`created_at`,`updated_at`)
+values(7,24,now(),now());
+
+INSERT INTO t_shirt_image(`t_shirt_id`,`image_id`,`created_at`,`updated_at`)
+values(7,25,now(),now());
+
+INSERT INTO t_shirt_image(`t_shirt_id`,`image_id`,`created_at`,`updated_at`)
+values(7,26,now(),now());
+
+INSERT INTO t_shirt_image(`t_shirt_id`,`image_id`,`created_at`,`updated_at`)
+values(7,27,now(),now());
